7a2cf8fa7a75bb7b5dea59a5f057926c14285d26
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-18 00:59:49 +0900

    surface

--- src/PCLSurface.jl
+++ src/PCLSurface.jl
@@ -1,5 +1,15 @@
 module PCLSurface
 
-# package code goes here
+using LibPCL
+using PCLCommon
+using Cxx
+
+const libpcl_surface = LibPCL.find_library_e("libpcl_surface")
+try
+    Libdl.dlopen(libpcl_surface, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
 
 end # module
